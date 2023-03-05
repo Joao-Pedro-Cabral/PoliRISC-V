a90efab66bda27fb53517cc8d56832300005798e
Joao <miranda.jp@usp.br>
2023-03-05 15:48:14 -0300

    Correções no tb da UC

--- testbench/core/ControlUnit/control_unit_tb.v
+++ testbench/core/ControlUnit/control_unit_tb.v
@@ -47,12 +47,13 @@ module control_unit_tb();
     wire [63:0] pc_in;
     wire [63:0] pc;
     // Sinais intermediários de teste
-    wire [41:0] LUT_uc [48:0];    // UC simulada com tabela
-    wire [24:0] df_src;           // Produzido pelo LUT
-    wire [24:0] db_df_src;        // Produzido pela UC
-    wire [63:0] immediate;        // imediato 
+    wire [41:0]   LUT_uc [48:0];    // UC simulada com tabela
+    wire [2057:0] LUT_linear;       // Tabela acima linearizada
+    wire [24:0]   df_src;           // Produzido pelo LUT
+    wire [24:0]   db_df_src;        // Produzido pela UC
+    wire [63:0]   immediate;        // imediato 
     // variáveis
-    integer program_size = 50;    // tamanho do programa que será executado
+    integer program_size = 1000;  // tamanho do programa que será executado
     integer i;
 
     // DUT
@@ -66,8 +67,8 @@ module control_unit_tb();
     ImmediateExtender extensor_imediato (.immediate(immediate), .instruction(instruction));
 
    // Instruction Memory
-    ROM #(.rom_init_file("./MIFs/core/ControlUnit/uc_tb.mif"), .word_size(32), .addr_size(8), .offset(2), .busy_time(12)) Instruction_Memory (.clock(clock), 
-                            .enable(instruction_mem_enable), .addr(instruction_address[7:0]), .data(instruction), .busy(instruction_mem_busy));
+    ROM #(.rom_init_file("./MIFs/core/RV64I/power.mif"), .word_size(32), .addr_size(10), .offset(2), .busy_time(12)) Instruction_Memory (.clock(clock), 
+                            .enable(instruction_mem_enable), .addr(instruction_address[9:0]), .data(instruction), .busy(instruction_mem_busy));
     assign opcode = instruction[6:0];
     assign funct3 = instruction[14:12];
     assign funct7 = instruction[31:25];
@@ -85,47 +86,54 @@ module control_unit_tb();
         #3;
     end
 
+    // geração do LUT linear
+    generate 
+        for(j = 0; j < 49; j = j + 1)
+            assign LUT_linear[42*(j+1)-1:42*j] = LUT_uc[j];
+    endgenerate
+
     // função para determinar os seletores a partir do opcode, funct3 e funct7
-    function [24:0] find_instruction;
-        input wire [6:0] opcode;
-        input wire [2:0] funct3;
-        input wire [6:0] funct7;
-        input wire [41:0] LUT_uc [48:0];
-        wire [24:0] source;
-        integer i;
-        // U,J : apenas opcode
-        if(opcode === 7'b0110111 || opcode === 7'b0010111 || opcode === 7'b1101111) begin
-            for(i = 0; i < 3; i = i + 1) begin
-                if(opcode === LUT_uc[i][6:0])
-                    source = LUT_uc[i][41:17];
+    function [24:0] find_instruction(input [6:0] opcode, input [2:0] funct3, input [6:0] funct7, input [2057:0] LUT_linear); 
+            integer i;
+            reg [24:0] temp;
+        begin
+            // U,J : apenas opcode
+            if(opcode === 7'b0110111 || opcode === 7'b0010111 || opcode === 7'b1101111) begin
+                for(i = 0; i < 3; i = i + 1)
+                    if(opcode == LUT_linear[35+42*i+:7])
+                        temp = LUT_linear[42*i+:25];
             end
-        end
-        // I, S, B: opcode e funct3
-        else if(opcode === 7'b1100011 || opcode === 7'b0000011 || opcode === 7'b0100011 || 
-           opcode === 7'b0010011 || opcode === 7'b0011011) begin
-            for(i = 3; i < 34; i = i + 1) begin
-                if(opcode === LUT_uc[i][6:0] && funct3 === LUT_uc[i][9:7])
-                    // SRLI e SRAI: funct7
-                    if(funct3 === 3'b101)
-                        if(funct7 === LUT_uc[i][16:10])
-                            source = LUT_uc[i][41:17];
-                    else
-                        source = LUT_uc[i][41:17];
+            // I, S, B: opcode e funct3
+            else if(opcode === 7'b1100011 || opcode === 7'b0000011 || opcode === 7'b0100011 || 
+                opcode === 7'b0010011 || opcode === 7'b0011011 || opcode === 7'b1100111) begin
+                for(i = 3; i < 34; i = i + 1) begin
+                    if(opcode === LUT_linear[35+42*i+:7] && funct3 === LUT_linear[32+42*i+:3]) begin
+                        // SRLI e SRAI: funct7
+                        if(funct3 === 3'b101) begin
+                            if(funct7[6:1] === LUT_linear[26+42*i+:6])
+                                temp = LUT_linear[42*i+:25];
+                        end
+                        else
+                            temp = LUT_linear[42*i+:25];
+                    end
+                end
             end
-        end
-        // R: opcode, funct3 e funct7
-        else if(opcode === 7'b0111011 || opcode === 7'b0110011 || opcode === 7'b1100111) begin
-            for(i = 34; i < 49; i = i + 1) begin
-                if(opcode === LUT_uc[i][6:0] && funct3 === LUT_uc[i][9:7] && funct7 === LUT_uc[i][16:10])
-                    source = LUT_uc[i][41:17];
+            // R: opcode, funct3 e funct7
+            else if(opcode === 7'b0111011 || opcode === 7'b0110011) begin
+               for(i = 34; i < 49; i = i + 1)
+                    if(opcode === LUT_linear[35+42*i+:7] && funct3 === LUT_linear[32+42*i+:3] && funct7 === LUT_linear[25+42*i+:7])
+                        temp = LUT_linear[42*i+:25];
             end
+            else begin
+                $display("Function error: opcode = %b", opcode);
+                $stop;
+            end
+            //$display("temp: %b", temp);
+            find_instruction = temp;
         end
-        else
-            $display("Function error: opcode = %b", opcode);
-        find_instruction = source;
     endfunction
 
-    assign db_df_src = {data_mem_byte_write_enable, data_mem_enable, write_register_enable, write_register_src, read_data_src, pc_src, alupc_src, arithmetic, carry_in, alu_src, aluy_src, alub_src, alua_src};
+    assign db_df_src = {alua_src, alub_src, aluy_src, alu_src, carry_in, arithmetic, alupc_src, pc_src, read_data_src, write_register_src, write_register_enable, data_mem_enable, data_mem_byte_write_enable};
 
     // testar o DUT
     initial begin: Testbench
@@ -153,79 +161,109 @@ module control_unit_tb();
             // Fetch
             if(pc_enable !== 1'b0 || write_register_enable !== 1'b0 || instruction_mem_enable !== 1'b1)
                 $display("Error Fetch: pc_enable = %b, write_register_enable = %b, instruction_mem_enable = %b", pc_enable, write_register_enable, instruction_mem_enable);
-            #17;
+            wait (instruction_mem_busy == 1'b1);
+            wait (instruction_mem_busy == 1'b0);
+            #3;
             // Decode
-            df_src = find_instruction(opcode, funct3, funct7, LUT_uc);
+            df_src = find_instruction(opcode, funct3, funct7, LUT_linear);
             #1;
             if(pc_enable !== 1'b0 || write_register_enable !== 1'b0 || instruction_mem_enable !== 1'b0)
                 $display("Error End-Fetch: pc_enable = %b, write_register_enable = %b, instruction_mem_enable = %b", pc_enable, write_register_enable, instruction_mem_enable);
-            // Execute -> Não testo pc_src para instruções do tipo B 
-            if(df_src[17:10] !== db_df_src[17:10] || df_src[8:0] !== db_df_src[8:0] || (df_src[9] !== db_df_src[9] && opcode !== 7'b1100011))
+            #6;
+            // Execute -> Não testo pc_src para instruções do tipo B e write_register_enable para Load
+            if({df_src[24:16], df_src[14:10], df_src[8:0]} !== {db_df_src[24:16], df_src[14:10], df_src[8:0]} || (df_src[15] !== db_df_src[15] && opcode !== 7'b1100011) || (df_src[9] !== db_df_src[9] && opcode !== 7'b0000011))
                 $display("Error Execute: df_src = %b, db_df_src = %b", df_src, db_df_src);
             // Testar pc_enable e incrementar pc
             case(opcode)
                 // Store(S*) e Load(L*)
-                7'b0100011, 7'b0000011:
+                7'b0100011, 7'b0000011: begin
                     pc_in = pc + 4;
-                    #14;
-                    if(pc_enable !== 1'b1)
-                        $display("PC enable error: pc_enable = %b, opcode = %b", pc_enable, opcode);
+                    wait (data_mem_busy == 1'b1);
+                    wait (data_mem_busy == 1'b0);
                     #4;
+                    if(pc_enable !== 1'b1 || write_register_enable !== df_src[9]) begin
+                        $display("Store/Load Error: pc_enable = %b, write_register_enable = %b, opcode = %b", pc_enable, write_register_enable, opcode);
+                        $stop;
+                    end
+                    #6;
+                end
                 // Branch(B*)
-                7'b1100011:
+                7'b1100011: begin
                     // testo pc_src de acordo com as flags aleatórias
-                    if(funct3[2:1] === 2'b00)
+                    if(funct3[2:1] === 2'b00) begin
                         if(~(zero ^ funct3[0]) === 1'b1) begin
                             pc_in = pc + immediate;
-                            if(pc_src !== 1'b1)
+                            if(pc_src !== 1'b1) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                         end
                         else begin
-                            if(pc_src !== 1'b0)
+                            if(pc_src !== 1'b0) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                             pc = pc + 4;
                         end
-                    else if(funct3[2:1] === 2'b10)
+                    end
+                    else if(funct3[2:1] === 2'b10) begin
                         if(~(negative ^ overflow ^ funct3[0]) === 1'b1) begin
                             pc_in = pc + immediate;
-                            if(pc_src !== 1'b1)
+                            if(pc_src !== 1'b1) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                         end
                         else begin
                             pc_in = pc + 4;
-                            if(pc_src !== 1'b0)
+                            if(pc_src !== 1'b0) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                         end
-                    else if(funct3[2:1] === 2'b11)
+                    end
+                    else if(funct3[2:1] === 2'b11) begin
                         if(carry_out ^ funct3[0] === 1'b1) begin
                             pc_in = pc + immediate;
-                            if(pc_src !== 1'b1)
+                            if(pc_src !== 1'b1) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                         end
                         else begin
                             pc_in = pc + 4;
-                            if(pc_src !== 1'b0)
+                            if(pc_src !== 1'b0) begin
                                 $display("Error B-type: pc_src = %b, funct3 = %b", pc_src, funct3);
+                                $stop;
+                            end
                         end
+                    end
                     else
                         $display("Error B-type: Invalid funct3! Funct3 : %b", funct3);
                     #6;
+                end
                 // JAL e JALR
-                7'b1101111, 7'b1100111:
-                    if(pc_enable !== 1'b1)
+                7'b1101111, 7'b1100111: begin
+                    if(pc_enable !== 1'b1) begin
                         $display("PC enable error: pc_enable = %b, opcode = %b", pc_enable, opcode);
+                        $stop;
+                    end
                     pc_in = pc + immediate;
                     #6;
-                // U/R/I-type
-                7'b0010011, 7'b0110011, 7'b0011011, 7'b0111011, 7'b0110111, 7'b0010111:
-                    if(pc_enable !== 1'b1)
+                end
+                // U-type & ULA R/I-type
+                7'b0010011, 7'b0110011, 7'b0011011, 7'b0111011, 7'b0110111, 7'b0010111: begin
+                    if(pc_enable !== 1'b1) begin
                         $display("PC enable error: pc_enable = %b, opcode = %b", pc_enable, opcode);
+                        $stop;
+                    end
                     pc_in = pc + 4;
                     #6;
-                default:
+                end
+                default: begin
                     $display("Error opcode case: opcode = %b", opcode);
-                    #6;
+                    $stop;
+                end
             endcase
         end
-
 endmodule
