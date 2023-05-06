bf6ca83eecf0f60c31ef2db4e6f4d7393cc29573
Igor Pontes Tresolavy <igor.tresolavy@controlid.com.br>
2023-05-06 14:31:52 -0300

    Adiciona primeira versão da Cache

--- rtl/memory/Controller/memory_controller.v
+++ rtl/memory/Controller/memory_controller.v
@@ -9,11 +9,11 @@
 `timescale 1 ns / 100 ps
 module memory_controller
 (
-  /* Interface com a memória ROM */
-  input  [63:0] rom_data,
-  input  rom_busy,
-  output wire rom_enable,
-  output wire [63:0] rom_addr,
+  /* Interface com o cache de instruções */
+  input  [63:0] inst_cache_data,
+  input  inst_cache_busy,
+  output inst_cache_enable,
+  output [63:0] inst_cache_addr,
   /* //// */
   
   /* Interface com a memória RAM */
@@ -33,25 +33,25 @@ module memory_controller
   /* //// */
 
   /* Interface com o processador */
-  input  wire mem_rd_en,         
-  input  wire mem_wr_en,         
-  input  wire [7:0] mem_byte_en, 
-  input  wire [63:0] wr_data,       
-  input  wire [63:0] mem_addr,      
+  input  mem_rd_en,         
+  input  mem_wr_en,         
+  input  [7:0] mem_byte_en, 
+  input  [63:0] wr_data,       
+  input  [63:0] mem_addr,      
 
-  output wire [63:0] rd_data,
-  output wire mem_busy
+  output [63:0] rd_data,
+  output mem_busy
   /* //// */
 
 );
 
   /* Sinais de controle */
-  wire   s_rom_enable          = mem_addr[63:24] == 0                            ? 1'b1 : 1'b0; // 16 MiB para a ROM
+  wire   s_rom_enable          = mem_addr[63:24] == 0                               ? 1'b1 : 1'b0; // 16 MiB para a ROM
   wire   s_ram_chip_select     = mem_addr[63:24] <= 'b100 && mem_addr[63:24] >= 'b1 ? 1'b1 : 1'b0; // 64 MiB para a RAM
-  assign rom_enable            = s_rom_enable & mem_rd_en;
+  assign inst_cache_enable     = s_rom_enable & mem_rd_en;
   assign ram_chip_select       = s_ram_chip_select;
 
-  assign mem_busy              = s_rom_enable ? rom_busy : s_ram_chip_select ? ram_busy : 1'b0;
+  assign mem_busy              = s_rom_enable ? inst_cache_busy : s_ram_chip_select ? ram_busy : 1'b0;
 
   assign ram_output_enable     = s_ram_chip_select             ? mem_rd_en   : 1'b0;
   assign ram_write_enable      = s_ram_chip_select             ? mem_wr_en   : 1'b0;
@@ -59,7 +59,7 @@ module memory_controller
   /* //// */
 
   /* Endereçamento */
-  assign rom_addr    = {40'h0, mem_addr[23:0]};
+  assign inst_cache_addr    = {40'h0, mem_addr[23:0]};
 
   wire ram_address24 = (~mem_addr[24])&(mem_addr[26]^mem_addr[25]);
   wire ram_address25 = (~mem_addr[26])&(mem_addr[25]&mem_addr[24])|
@@ -71,7 +71,7 @@ module memory_controller
   genvar i;
   generate
     for(i=0; i < 8; i = i + 1) begin
-      assign rd_data[(i+1)*8-1 -: 8] = s_rom_enable ? rom_data[(i+1)*8-1 -: 8] : (s_ram_chip_select ? ram_read_data[(i+1)*8-1 -: 8] : 64'b0); 
+      assign rd_data[(i+1)*8-1 -: 8] = s_rom_enable ? inst_cache_data[(i+1)*8-1 -: 8] : (s_ram_chip_select ? ram_read_data[(i+1)*8-1 -: 8] : 64'b0); 
     end
   endgenerate
   /* //// */
