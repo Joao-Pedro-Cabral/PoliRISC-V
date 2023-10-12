d7e4461c6551e277e58208d010dc61dceced6f8c
Igor Pontes Tresolavy <tresolavy@usp.br>
2023-10-12 18:55:29 -0300

    atualiza mtvec no ecall.s

--- simulation/assembly_converter/ecall.s
+++ simulation/assembly_converter/ecall.s
@@ -5,7 +5,7 @@ addi sp,sp,-32
 addi s0,sp,32
 
 ; writing to mtvec
-addi t0,x0,168            ; BASE address for interrupt handling
+addi t0,x0,64             ; BASE address for interrupt handling
 csrrw x0,mtvec,t0
 csrrw x0,stvec,t0
 
@@ -14,8 +14,6 @@ lui t0,-1
 lui t1,262143             ; mtimecmp base address
 sw t0,0(t1)
 
-addi a6,x0,0b1001         ; SEI
-addi a7,x0,0b1011         ; MEI
 ori t0,x0,0b101010101010
 csrrw x0,mie,t0           ; enables every interrupt
 ori t1,x0,0b10101010
