4a47b9e86c445c3a51597d18430552f744cdcd5b
João Pedro Cabral Miranda <miranda.jp@usp.br>
2024-01-19 23:01:49 -0300

    Cache de instrucoes com wishbone

--- rtl/memory/Cache/instruction_cache.v
+++ rtl/memory/Cache/instruction_cache.v
@@ -6,32 +6,36 @@
 //
 
 module instruction_cache #(
-    parameter integer L2_CACHE_SIZE = 8,  // log_2(tamanho da cache em bytes)
-    parameter integer L2_BLOCK_SIZE = 6,  // log_2(tamanho do bloco em bytes)
-    parameter integer L2_ADDR_SIZE  = 5,  // log2(bits de endereço)
-    parameter integer L2_DATA_SIZE  = 2   // log2(bytes de dados)
+    parameter integer L2_CACHE_SIZE = 8,  // log2(tamanho da cache em bytes)
+    parameter integer L2_BLOCK_SIZE = 6,  // log2(tamanho do bloco em bytes)
+    parameter integer L2_ADDR_SIZE  = 32, // log2(tamanho do endereço em bits)
+    parameter integer L2_DATA_SIZE  = 2   // log2(tamanho do dados em bytes)
 ) (
     /* Sinais do sistema */
-    input CLK_I,
-    input RST_I,
+    input wire CLK_I,
+    input wire RST_I,
     /* //// */
 
     /* Interface com a memória de instruções */
-    input [2**(L2_BLOCK_SIZE+3)-1:0] inst_DAT_I,
-    input inst_ACK_I,
-    output inst_CYC_O,
-    output [2**L2_ADDR_SIZE-1:0] inst_ADR_O,
+    input  wire [2**(L2_BLOCK_SIZE+3)-1:0] inst_DAT_I,
+    input  wire inst_ACK_I,
+    output wire inst_CYC_O,
+    output wire inst_STB_O,
+    output wire [L2_ADDR_SIZE-1:0] inst_ADR_O,
     /* //// */
 
     /* Interface com o controlador de memória */
-    input inst_cache_CYC_I,
-    input [2**L2_ADDR_SIZE-1:0] inst_cache_ADR_I,
-    output [2**(L2_DATA_SIZE+3)-1:0] inst_cache_DAT_O,
-    output inst_cache_ACK_O
+    input  wire inst_cache_CYC_I,
+    input  wire inst_cache_STB_I,
+    input  wire [L2_ADDR_SIZE-1:0] inst_cache_ADR_I,
+    output wire [2**(L2_DATA_SIZE+3)-1:0] inst_cache_DAT_O,
+    output wire inst_cache_ACK_O
     /* //// */
 
 );
 
+  // TGC: represents cache hit
+  // WE: represents cache write
   wire TGC_I, TGC_O, cache_WE_I, cache_WE_O;
 
   instruction_cache_control control (
@@ -39,7 +43,9 @@ module instruction_cache #(
       .RST_I(RST_I),
       .inst_ACK_I(inst_ACK_I),
       .inst_CYC_O(inst_CYC_O),
+      .inst_STB_O(inst_STB_O),
       .inst_cache_CYC_I(inst_cache_CYC_I),
+      .inst_cache_STB_I(inst_cache_STB_I),
       .inst_cache_ACK_O(inst_cache_ACK_O),
       .TGC_I(TGC_I),
       .cache_WE_O(cache_WE_O)
@@ -51,6 +57,7 @@ module instruction_cache #(
       .L2_ADDR_SIZE (L2_ADDR_SIZE),
       .L2_DATA_SIZE (L2_DATA_SIZE)
   ) path (
+      .CLK_I(CLK_I),
       .RST_I(RST_I),
       .inst_DAT_I(inst_DAT_I),
       .inst_ADR_O(inst_ADR_O),
--- testbench/memory/Cache/instruction_cache_tb.v
+++ testbench/memory/Cache/instruction_cache_tb.v
@@ -6,30 +6,25 @@
 //! @date   2023-05-02
 //
 
-`timescale 1 ns / 100 ps
+`define ASSERT(cond, message) if (!(cond)) begin $display (message) ; $stop; end
 
-`define ASSERT(condition, message) \
-  if (!(condition)) \
-  begin \
-    $display(message); \
-    $stop; \
-  end
+`timescale 1 ns / 100 ps
 
 module instruction_cache_tb ();
 
   localparam integer AmntOfTests = 10000;
   localparam integer ClockPeriod = 10;
-  localparam integer BusyCycles = 3;
+  localparam integer AckCycles = 3;
 
   localparam integer L2CacheSize = 4;
   localparam integer L2BlockSize = 2;
-  localparam integer L2AddrSize = 3;
+  localparam integer L2AddrSize = 32;
   localparam integer L2DataSize = 1;
   localparam integer Offset = L2BlockSize;
   localparam integer ByteOffset = L2DataSize;
   localparam integer BlockOffset = L2BlockSize - L2DataSize;
-  localparam integer Index = L2CacheSize - Offset;
-  localparam integer Tag = 2 ** (L2DataSize + 3) - Offset - Index;
+  localparam integer Index = L2CacheSize - L2BlockSize;
+  localparam integer Tag = L2AddrSize - Offset - Index;
   localparam integer Depth = 2 ** (L2CacheSize - L2BlockSize);
 
   /* Sinais do sistema */
@@ -41,12 +36,14 @@ module instruction_cache_tb ();
   reg [2**(L2BlockSize+3)-1:0] inst_DAT_I;
   reg inst_ACK_I;
   wire inst_CYC_O;
-  wire [2**L2AddrSize-1:0] inst_ADR_O;
+  wire inst_STB_O;
+  wire [L2AddrSize-1:0] inst_ADR_O;
   /* //// */
 
   /* Interface com o controlador de memória */
   reg inst_cache_CYC_I;
-  reg [2**L2AddrSize-1:0] inst_cache_ADR_I;
+  reg inst_cache_STB_I;
+  reg [L2AddrSize-1:0] inst_cache_ADR_I;
   wire [2**(L2DataSize+3)-1:0] inst_cache_DAT_O;
   wire inst_cache_ACK_O;
   /* //// */
@@ -58,20 +55,18 @@ module instruction_cache_tb ();
 
   // Sinais intermediários
   wire [Index-1:0] index = inst_cache_ADR_I[Index+Offset-1:Offset];
-  wire [Tag-1:0] tag = inst_cache_ADR_I[2**L2AddrSize-1:Index+Offset];
+  wire [Tag-1:0] tag = inst_cache_ADR_I[L2AddrSize-1:Index+Offset];
   wire [(Offset>0 ? Offset-1 : 0):0] offset = Offset > 0 ? inst_cache_ADR_I[Offset-1:0] : 0;
-  wire [(ByteOffset>0 ? ByteOffset-1 : 0):0] byte_offset = ByteOffset > 0 ? offset[ByteOffset-1:0] : 0;
-  wire [(BlockOffset>0 ? BlockOffset-1 : 0):0] block_offset = BlockOffset > 0 ? offset[Offset-1:ByteOffset] : 0;
-  event busy_event;
-  always @(busy_event) begin
-    inst_ACK_I = ~inst_ACK_I;
-  end
+  wire [(ByteOffset>0 ? ByteOffset-1 : 0):0] byte_offset = ByteOffset > 0
+                                                ? offset[ByteOffset-1:0] : 0;
+  wire [(BlockOffset>0 ? BlockOffset-1 : 0):0] block_offset = BlockOffset > 0
+                                                ? offset[Offset-1:ByteOffset] : 0;
 
   instruction_cache #(
-      .L2_CACHE_SIZE(L2CacheSize),  // log_2(tamanho da cache em bytes)
-      .L2_BLOCK_SIZE(L2BlockSize),  // log_2(tamanho do bloco em bytes)
-      .L2_ADDR_SIZE (L2AddrSize),   // log2(bits de endereço)
-      .L2_DATA_SIZE (L2DataSize)    // log2(bytes de dados)
+      .L2_CACHE_SIZE(L2CacheSize),  // log2(tamanho da cache em bytes)
+      .L2_BLOCK_SIZE(L2BlockSize),  // log2(tamanho do bloco em bytes)
+      .L2_ADDR_SIZE (L2AddrSize),   // log2(tamanho do endereço em bits)
+      .L2_DATA_SIZE (L2DataSize)    // log2(tamanho do dados em bytes)
   ) DUT (
       /* Sinais do sistema */
       .CLK_I(CLK_I),
@@ -81,76 +76,75 @@ module instruction_cache_tb ();
       /* Interface com a memória de instruções */
       .inst_DAT_I  (inst_DAT_I),
       .inst_ACK_I  (inst_ACK_I),
-      .inst_CYC_O(inst_CYC_O),
+      .inst_CYC_O  (inst_CYC_O),
+      .inst_STB_O  (inst_STB_O),
       .inst_ADR_O  (inst_ADR_O),
       /* //// */
 
       /* Interface com o controlador de memória */
-      .inst_cache_CYC_I(inst_cache_CYC_I),
+      .inst_cache_CYC_I  (inst_cache_CYC_I),
+      .inst_cache_STB_I  (inst_cache_STB_I),
       .inst_cache_ADR_I  (inst_cache_ADR_I),
       .inst_cache_DAT_O  (inst_cache_DAT_O),
       .inst_cache_ACK_O  (inst_cache_ACK_O)
       /* //// */
   );
 
-  task assert_dut;
+  task automatic assert_dut(input reg [2**(L2DataSize+3)-1:0] data,
+    input reg [Tag-1:0] tag, input reg valid, input reg cyc_stb, input reg we, input reg ack);
     begin
-      `ASSERT(inst_cache_ACK_O === 1'b1, "\tinst_cache_busy !== 0b0")
-      `ASSERT(
-          cache_data[index][(block_offset+1)*(2**(ByteOffset+3))-1-:(2**(ByteOffset+3))] === inst_cache_DAT_O,
-          "\tcache_data[index][(block_offset+1)*(2**(ByteOffset+3))-1-:(2**(ByteOffset+3))] !== inst_cache_DAT_O")
-      `ASSERT(cache_tag[index] === DUT.path.cache_tag[index],
-              "\tcache_tag[index] !== DUT.path.cache_tag[index]")
-      `ASSERT(cache_valid[index] === DUT.path.cache_valid[index],
-              "\tcache_valid[index] !== DUT.path.cache_valid[index]")
-      `ASSERT(DUT.control.cache_WE_O === '0, "\tDUT.control.cache_WE_O !== 0b0")
+      `ASSERT(inst_cache_ACK_O === ack, "\tinst_cache_ack !== ack")
+      `ASSERT(inst_cache_DAT_O === data,
+          "\tinst_cache_DAT_O !== data")
+      `ASSERT(tag === DUT.path.cache_tag[index],
+              "\tDUT.path.cache_tag[index] !== index")
+      `ASSERT(valid === DUT.path.cache_valid[index],
+              "\tDUT.path.cache_valid[index] !== valid")
+      `ASSERT(inst_CYC_O === cyc_stb, "\tinst_CYC_O !== cyc_stb")
+      `ASSERT(inst_STB_O === cyc_stb, "\tinst_STB_O !== cyc_stb")
+      `ASSERT(DUT.control.cache_WE_O === we, "\tDUT.control.cache_WE_O !== we")
     end
   endtask
 
-  task doNothing(input integer test_case);
+  task automatic doNothing(input integer test_case);
     begin
       $display("[%0t] doNothing start: test %d", $time, test_case);
       #ClockPeriod;
       $display("[%0t] doNothing end", $time);
-      assert_dut();
-      @(negedge CLK_I);
+      assert_dut(cache_data[index][(block_offset+1)*(2**(ByteOffset+3))-1-:(2**(ByteOffset+3))],
+                 cache_tag[index], cache_valid[index], 1'b0, 1'b0, 1'b0);
     end
   endtask
 
-  task hitState(input integer test_case);
+  task automatic hitState(input integer test_case);
     begin
       $display("[%0t] hitState start: test %d", $time, test_case);
-      `ASSERT(inst_cache_ACK_O === 1'b0, "\tinst_cache_busy !== 0b1")
+      `ASSERT(inst_cache_ACK_O === 1'b0, "\tinst_cache_ack !== 0")
       #ClockPeriod;
       $display("[%0t] hitState end", $time);
-      assert_dut();
-      @(negedge CLK_I);
+      assert_dut(cache_data[index][(block_offset+1)*(2**(ByteOffset+3))-1-:(2**(ByteOffset+3))],
+                 cache_tag[index], 1'b1, 1'b0, 1'b0, 1'b1);
     end
   endtask
 
-  task missState(input integer test_case);
+  task automatic missState(input integer test_case);
     begin
       $display("[%0t] missState start: test %d", $time, test_case);
-      `ASSERT(inst_cache_ACK_O === 1'b0, "\tinst_cache_busy !== 0b1")
-      /* inst_ACK_I = 1'b1; */
-      -> busy_event;
-      `ASSERT(DUT.control.cache_WE_O === inst_ACK_I,
-              "\tDUT.control.cache_WE_O !== inst_ACK_I")
-      fork
-        begin
-          #(BusyCycles * ClockPeriod) -> busy_event;
-        end
-        begin
-          #(BusyCycles / 2.0 * ClockPeriod);
-          cache_data[index]  = inst_DAT_I;
-          cache_tag[index]   = tag;
-          cache_valid[index] = 1'b1;
-        end
-      join
-      #(ClockPeriod);
+      `ASSERT(inst_cache_ACK_O === 1'b0, "\tinst_cache_ack !== 0")
+      #ClockPeriod;
+      `ASSERT(inst_CYC_O === 1'b1, "\tinst_cyc !== 1")
+      `ASSERT(inst_STB_O === 1'b1, "\tinst_stb !== 1")
+      #(AckCycles / 2.0 * ClockPeriod);
+      cache_data[index]  = inst_DAT_I;
+      cache_tag[index]   = tag;
+      cache_valid[index] = 1'b1;
+      #(AckCycles / 2.0 * ClockPeriod);
       $display("[%0t] missState end", $time);
-      assert_dut();
-      @(negedge CLK_I);
+      inst_ACK_I = 1'b1;
+      #ClockPeriod;
+      assert_dut(cache_data[index][(block_offset+1)*(2**(ByteOffset+3))-1-:(2**(ByteOffset+3))],
+                 cache_tag[index], 1'b1, 1'b0, 1'b0, 1'b1);
+      inst_ACK_I = 1'b0;
     end
   endtask
 
@@ -158,8 +152,8 @@ module instruction_cache_tb ();
   always #(ClockPeriod/2) CLK_I = ~CLK_I;
   integer i;
   initial begin
-    {CLK_I, RST_I, inst_DAT_I, inst_cache_CYC_I, inst_cache_ADR_I, cache_valid} = 0;
-    inst_ACK_I = 1'b1;
+    {CLK_I, RST_I, inst_DAT_I, inst_cache_CYC_I, inst_cache_STB_I,
+    inst_ACK_I, inst_cache_ADR_I, cache_valid} = 0;
 
     @(negedge CLK_I);
     RST_I = 1;
@@ -169,14 +163,15 @@ module instruction_cache_tb ();
     $display("[%0t] SOT", $time);
 
     // Teste de leitura da Cache
-    @(negedge CLK_I);
     for (i = 0; i < AmntOfTests; i = i + 1) begin
+      @(negedge CLK_I);
       inst_DAT_I = $urandom;
       inst_cache_CYC_I = $urandom;
+      inst_cache_STB_I = $urandom;
       inst_cache_ADR_I = $urandom;
       @(negedge CLK_I);
 
-      if (inst_cache_CYC_I === 1'b1) begin
+      if (inst_cache_CYC_I && inst_cache_STB_I) begin
         if (cache_valid[index] === 1'b1 && tag === cache_tag[index]) begin
           hitState(i);
         end else begin
