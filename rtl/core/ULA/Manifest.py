5f41952f1e020b3f9d5be686997841c7ecfa16d4
João Pedro Cabral Miranda <miranda.jp@usp.br>
2024-01-24 22:45:23 -0300

    Reorganizando diretorio ROM dos MIFs e criando script para rodar todos os testbenchs

--- rtl/core/ULA/Manifest.py
+++ rtl/core/ULA/Manifest.py
@@ -7,6 +7,7 @@ files = [
 
 modules = {
     "local": [
+        "../Adder",
         "../../../utils/components"
     ],
 }
---
+++ simulation/auto_test/auto_test.py
@@ -0,0 +1,40 @@
+
+import subprocess
+import sys
+from manifest_utils import set_macros, run_simulation, write_lines
+from defines import tops_with_mifs
+
+
+def find_files(root_directory: str, extension: str):
+    # Use the find command to locate files
+    command = ['find', root_directory, '-type', 'f', '-name', f'*.{extension}']
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        files = result.stdout.strip().split('\n')
+        # Obtain only the file name
+        return [file.split('/')[-1].split('.')[0] for file in files]
+    else:
+        print(f"Error executing find command: {result.stderr}")
+        return []
+
+
+# MAIN
+sys.stdout = open("../log.txt", "w")
+sim_top_array = find_files("../../testbench", "v")  # Get all testbenches files
+print("############## tops: " + str(sim_top_array))
+# Get mifs files for core simulation
+mif_array = find_files("../MIFs/memory/ROM/core", "mif")
+print("############## mifs: " + str(mif_array))
+
+write_lines(["gui_mode"], ["gui_mode = False"])  # Set TCL Mode
+
+for testbench in sim_top_array:
+    print(f'---------{testbench}---------')
+    write_lines(["sim_top"], [f"sim_top = \"{testbench}\""])  # Set TCL Mode
+    set_macros(testbench)
+    if tops_with_mifs.count(testbench) != 0:
+        for i, mif in enumerate(mif_array):
+            write_lines(["mif_name"], [f"mif_name = \"{mif}.mif\""])
+            run_simulation()
+    else:
+        run_simulation()
