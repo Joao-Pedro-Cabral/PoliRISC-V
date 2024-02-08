ab921984a7db020924bea3c69541c060123edd55
Igor Pontes Tresolavy <tresolavy@usp.br>
2024-02-08 00:43:35 -0300

    [WIP] TODO: utilizar instruções *W

--- simulation/assembly_converter/assembly/m_extension64.s
+++ simulation/assembly_converter/assembly/m_extension64.s
@@ -8,28 +8,28 @@ mul a2, a0, a1 # 0x1
 # MULH - Multiply High (signed)
 lui a0,-65536   # 0xFFFFFFFF_F0000000
 lui t0,1
-mul a0,a0,t0    # 0xF00_00000000
+mul a0,a0,t0    # 0xFFFFFF00_00000000
 lui a1,621412   
 mul a1,a1,t0    # 0xFFFFF97B_64000000
-mulh a3, a0, a1 # 0xFFFFF87B_6406849C
+mulh a3, a0, a1 # 0x00000000_0006849C
 
 # MULHSU - Multiply High (signed-unsigned)
-mulhsu a4, a3, a1 # 0x03DCDBEC = 64805868
+mulhsu a4, a3, a1 # 0x00000000_0006849B
 
 # MULHU - Multiply High (unsigned)
-mulhu a5, a4, a0  # 0x039F0E2D = 60755501
+mulhu a5, a4, a0  # 0x00000000_0006849A
 
 # DIV - Divide (signed)
-div a6, a1, a5 # 0xFFFFFFE4 = -28 = 4294967268
+div a6, a0, a5 # 0xFFFFFFFF_FEFFFFB2
 
 # DIVU - Divide (unsigned)
-divu a7, a6, a5 # 0x00000046 = 70
+divu a7, a6, a5 # 0x00002746_A82233F0
 
 # REM - Remainder (signed)
-rem a2, a0, a7 # 0xFFFFFFF0 = -16 = 4294967280
+rem a2, a0, a4  # 0xFFFFFFFF_FFFF4C16
 
 # REMU - Remainder (unsigned)
-remu a3, a2, a5 # 0x02821FA2 = 42082210
+remu a3, a2, a5 # 0x00000000_00061A40
 
 # Write data
 lui sp,4096
