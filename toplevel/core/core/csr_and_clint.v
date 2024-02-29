d70500aacda9fb8efb477f0401553a766218a36f
João Pedro Cabral Miranda <miranda.jp@usp.br>
2024-02-29 00:04:50 -0300

    Adicionando mais registradores ao csr_clint

--- toplevel/core/core/csr_and_clint.v
+++ toplevel/core/core/csr_and_clint.v
@@ -1,5 +1,4 @@
 `include "macros.vh"
-`include "extensions.vh"
 
 module csr_and_clint (
     // core interface
@@ -327,28 +326,125 @@ module csr_and_clint (
   end
   // leds block end
 
-  // leds block
-  localparam reg [31:0] LedsBase = 32'hf0003800;
-  localparam reg [31:0] LedsOut = 32'hf0003800;
+  // sdcard_block2mem block
+  localparam reg [31:0] SdCardBlock2MemBase = 32'hf0004000;
+  localparam reg [2:0] SdCardBlock2MemSize = 7;
 
-  reg [7:0] leds_out;
+  reg [31:0] sdcard_block2mem_out [SdCardBlock2MemSize-1:0];
 
   always @(posedge CLK_I, posedge RST_I) begin
     if (1'b1 == RST_I) begin
-      leds_out <= 8'b0;
-    end else if (adr_i_is_base(LedsBase)) begin
-      case (1'b1)
-        adr_i_reg_sel(
-            LedsOut
-        ): begin
-          leds_out <= DAT_I[7:0];
-        end
-        default: begin
-        end
-      endcase
+      sdcard_block2mem_out <= {SdCardBlock2MemSize{0}};
+    end else if (adr_i_is_base(SdCardBlock2MemBase)) begin
+      sdcard_block2mem_out[ADR_I[4:2]] <= DAT_I;
     end
   end
-  // leds block end
+  // sdcard_block2mem block end
+
+  // sdcard_core block
+  localparam reg [31:0] SdCardCoreBase = 32'hf0004800;
+  localparam reg [3:0] SdCardCoreSize = 11;
+
+  reg [31:0] sdcard_core_out [SdCardCoreSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      sdcard_core_out <= {SdCardCoreSize{0}};
+    end else if (adr_i_is_base(SdCardCoreBase)) begin
+      sdcard_core_out[ADR_I[5:2]] <= DAT_I;
+    end
+  end
+  // sdcard_core block end
+
+  // sdcard_irq block
+  localparam reg [31:0] SdCardIrqBase = 32'hf0005000;
+  localparam reg [1:0] SdCardIrqSize = 3;
+
+  reg [31:0] sdcard_irq_out [SdCardIrqSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      sdcard_irq_out <= {SdCardIrqSize{0}};
+    end else if (adr_i_is_base(SdCardIrqBase)) begin
+      sdcard_irq_out[ADR_I[3:2]] <= DAT_I;
+    end
+  end
+  // sdcard_irq block end
+
+  // sdcard_mem2block block
+  localparam reg [31:0] SdCardMem2BlockBase = 32'hf0005800;
+  localparam reg [2:0] SdCardMem2BlockSize = 7;
+
+  reg [31:0] sdcard_mem2block_out [SdCardMem2BlockSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      sdcard_mem2block_out <= {SdCardMem2BlockSize{0}};
+    end else if (adr_i_is_base(SdCardMem2BlockBase)) begin
+      sdcard_mem2block_out[ADR_I[4:2]] <= DAT_I;
+    end
+  end
+  // sdcard_mem2block block end
+
+  // sdcard_phy block
+  localparam reg [31:0] SdCardPhyBase = 32'hf0006000;
+  localparam reg [1:0] SdCardPhySize = 4;
+
+  reg [31:0] sdcard_phy_out [SdCardPhySize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      sdcard_phy_out <= {SdCardPhySize{0}};
+    end else if (adr_i_is_base(SdCardPhyBase)) begin
+      sdcard_phy_out[ADR_I[3:2]] <= DAT_I;
+    end
+  end
+  // sdcard_phy block end
+
+  // sdram_dfii block
+  localparam reg [31:0] SdRamDfiiBase = 32'hf0006800;
+  localparam reg [3:0] SdRamDfiiSize = 13;
+
+  reg [31:0] sdram_dfii_out [SdRamDfiiSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      sdram_dfii_out <= {SdRamDfiiSize{0}};
+    end else if (adr_i_is_base(SdRamDfiiBase)) begin
+      sdram_dfii_out[ADR_I[5:2]] <= DAT_I;
+    end
+  end
+  // sdram_dfii block end
+
+  // video_dma block
+  localparam reg [31:0] SdVideoDmaBase = 32'hf0006800;
+  localparam reg [2:0] SdVideoDmaSize = 6;
+
+  reg [31:0] video_dma_out [SdVideoDmaSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      video_dma_out <= {SdVideoDmaSize{0}};
+    end else if (adr_i_is_base(SdVideoDmaBase)) begin
+      video_dma_out[ADR_I[4:2]] <= DAT_I;
+    end
+  end
+  // video_dma block end
+
+  // video_vtg block
+  localparam reg [31:0] SdVideoVtgBase = 32'hf0006800;
+  localparam reg [3:0] SdVideoVtgSize = 9;
+
+  reg [31:0] video_vtg_out [SdVideoVtgSize-1:0];
+
+  always @(posedge CLK_I, posedge RST_I) begin
+    if (1'b1 == RST_I) begin
+      video_vtg_out <= {SdVideoVtgSize{0}};
+    end else if (adr_i_is_base(SdVideoVtgBase)) begin
+      video_vtg_out[ADR_I[5:2]] <= DAT_I;
+    end
+  end
+  // video_vtg block end
 
   assign DAT_O = adr_i_is_base(Uart) ? _uart_DAT_O : _DAT_O;
   assign ACK_O = _ACK_O;
