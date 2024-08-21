a0a144a401ffe2b4b1015bfd997c66c9970fb67a
Igor Pontes Tresolavy <tresolavy@usp.br>
2024-08-21 14:48:14 -0300

    refactor: refactors forwarding unit

--- rtl/core/ForwardingUnit/forwarding_unit.sv
+++ rtl/core/ForwardingUnit/forwarding_unit.sv
@@ -2,7 +2,7 @@ import forwarding_unit_pkg::*;
 
 module forwarding_unit #(
     parameter integer N = 5
-  ) (
+) (
     input forwarding_type_t forwarding_type_id,
     input forwarding_type_t forwarding_type_ex,
     input forwarding_type_t forwarding_type_mem,
@@ -29,18 +29,18 @@ module forwarding_unit #(
     return reg_we && rs && rd == rs;
   endfunction
 
-  function automatic bit forward(input logic reg_we, input logic [N-1:0] rd,
-                                 input forwarding_t target_forwarding, input logic [N-1:0] rs,
-                                 output forwarding_t forward_rs);
+  function automatic forwarding_t forward(input logic reg_we, input logic [N-1:0] rd,
+                                          input forwarding_t target_forwarding,
+                                          input logic [N-1:0] rs);
     if (valid_forwarding(reg_we, rs, rd)) begin
-      forward_rs = target_forwarding;
-      return 1'b1;
+      return target_forwarding;
     end
-    forward_rs = NoForwarding;
-    return 1'b0;
+    return NoForwarding;
   endfunction
 
   always_comb begin : forward_rsx_id_proc
+    forward_rs1_id = NoForwarding;
+    forward_rs2_id = NoForwarding;
     unique case (forwarding_type_id)
 
       NoForward: begin
@@ -49,20 +49,24 @@ module forwarding_unit #(
       end
 
       ForwardExecute, ForwardExecuteMemory: begin
-        void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_id, forward_rs1_id));
-        void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_id, forward_rs2_id));
+        forward_rs1_id = forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_id);
+        forward_rs2_id = forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_id);
       end
 
       ForwardDecode: begin
-        if (!forward(reg_we_ex, rd_ex, ForwardFromEx, rs1_id, forward_rs1_id)) begin
-          if (!forward(reg_we_mem, rd_mem, ForwardFromMem, rs1_id, forward_rs1_id)) begin
-            void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_id, forward_rs1_id));
+        forward_rs1_id = forward(reg_we_ex, rd_ex, ForwardFromEx, rs1_id);
+        if (forward_rs1_id == NoForwarding) begin
+          forward_rs1_id = forward(reg_we_mem, rd_mem, ForwardFromMem, rs1_id);
+          if (forward_rs1_id == NoForwarding) begin
+            forward_rs1_id = forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_id);
           end
         end
 
-        if (!forward(reg_we_ex, rd_ex, ForwardFromEx, rs2_id, forward_rs2_id)) begin
-          if (!forward(reg_we_mem, rd_mem, ForwardFromMem, rs2_id, forward_rs2_id)) begin
-            void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_id, forward_rs2_id));
+        forward_rs2_id = forward(reg_we_ex, rd_ex, ForwardFromEx, rs2_id);
+        if (forward_rs2_id == NoForwarding) begin
+          forward_rs2_id = forward(reg_we_mem, rd_mem, ForwardFromMem, rs2_id);
+          if (forward_rs2_id == NoForwarding) begin
+            forward_rs2_id = forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_id);
           end
         end
       end
@@ -76,6 +80,8 @@ module forwarding_unit #(
   end : forward_rsx_id_proc
 
   always_comb begin : forward_rsx_ex_proc
+    forward_rs1_ex = NoForwarding;
+    forward_rs2_ex = NoForwarding;
     unique case (forwarding_type_ex)
 
       NoForward, ForwardDecode: begin
@@ -84,11 +90,13 @@ module forwarding_unit #(
       end
 
       ForwardExecute, ForwardExecuteMemory: begin
-        if (!forward(reg_we_mem, rd_mem, ForwardFromMem, rs1_ex, forward_rs1_ex)) begin
-          void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_ex, forward_rs1_ex));
+        forward_rs1_ex = forward(reg_we_mem, rd_mem, ForwardFromMem, rs1_ex);
+        if (forward_rs1_ex == NoForwarding) begin
+          forward_rs1_ex = forward(reg_we_wb, rd_wb, ForwardFromWb, rs1_ex);
         end
-        if (!forward(reg_we_mem, rd_mem, ForwardFromMem, rs2_ex, forward_rs2_ex)) begin
-          void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_ex, forward_rs2_ex));
+        forward_rs2_ex = forward(reg_we_mem, rd_mem, ForwardFromMem, rs2_ex);
+        if (forward_rs2_ex == NoForwarding) begin
+          forward_rs2_ex = forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_ex);
         end
       end
 
@@ -101,6 +109,7 @@ module forwarding_unit #(
   end : forward_rsx_ex_proc
 
   always_comb begin : forward_rs2_mem_proc
+    forward_rs2_mem = NoForwarding;
     unique case (forwarding_type_mem)
 
       NoForward, ForwardExecute, ForwardDecode: begin
@@ -108,7 +117,7 @@ module forwarding_unit #(
       end
 
       ForwardExecuteMemory: begin
-        void'(forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_mem, forward_rs2_mem));
+        forward_rs2_mem = forward(reg_we_wb, rd_wb, ForwardFromWb, rs2_mem);
       end
 
       default: begin
